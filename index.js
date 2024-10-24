320cfc428686aefc1e6010a935deccbec8f0d457
Joaquin Del Vecchio <172492638+JoaDelVecchio@users.noreply.github.com>
2024-10-24 09:37:39 -0300

    Fix route

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.get("/api/persons/:id", (request, response) => {
   }
 });
 
-app.delete("/api/person/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response) => {
   const id = request.params.id;
   persons = persons.filter((person) => person.id !== id);
   response.status(204).end();
